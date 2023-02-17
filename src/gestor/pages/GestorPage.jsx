058f6a4e74bfd21d37505b17fa78be31385fb15c
Antonella <antopiastri@hotmail.com>
2023-02-17 00:04:45 -0300

    Fix sobre color de botones

--- src/gestor/pages/GestorPage.jsx
+++ src/gestor/pages/GestorPage.jsx
@@ -33,17 +33,17 @@ export const GestorPage = () => {
             <Grid  container spacing={0} direction= "column" alignItems="center" justifyContent="center" sx={{ minHeight: "100vh", backgroundImage:`url("http://demos.alan.sh/files/images/ujkoW.jpg")`,backgroundSize: "cover", backgroundPosition:"10% 52%", padding:4 }}>
                 
                 <Grid item container spacing={0} direction= "column" alignItems="center" justifyContent="center">
-                    <Button variant="contained" sx={{marginBottom:5 , width:1/2}} color="mainPageButton" onClick={goToAnimalesPage}>Ver Animales</Button>
+                    <Button variant="contained" sx={{marginBottom:5 , width:1/2}} color="primary" onClick={goToAnimalesPage}>Ver Animales</Button>
                 </Grid>
                 <Grid container spacing={4} direction= "row" alignItems="center" justifyContent="center">
                     <Grid item >
-                        <Button variant="contained" color="mainPageButton" onClick={goToTPesoPage}>Registrar Pesada</Button>
+                        <Button variant="contained" color="primary" onClick={goToTPesoPage}>Registrar Pesada</Button>
                     </Grid>
                     <Grid item>
-                        <Button variant="contained" color="mainPageButton" onClick={goToTReproductivoPage}>Registrar Ecografías</Button>
+                        <Button variant="contained" color="primary" onClick={goToTReproductivoPage}>Registrar Ecografías</Button>
                     </Grid>
                     <Grid item>
-                        <Button variant="contained" color="mainPageButton" onClick={goToTSanitarioPage}>Registrar Sanitario</Button>
+                        <Button variant="contained" color="primary" onClick={goToTSanitarioPage}>Registrar Sanitario</Button>
                     </Grid>
                 </Grid>
                 
